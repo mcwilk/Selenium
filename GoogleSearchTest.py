05724982d1fdd1f1ccc7b75b89068ddcc809979b
mcwilk <mcwilk@wp.pl>
2023-11-10 22:22:58 +0100

    Add webdriver_manager and install chromedriver on the fly

--- GoogleSearchTest.py
+++ GoogleSearchTest.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-chrome_service = webdriver.ChromeService(r".\drivers\chromedriver.exe")
+chrome_service = webdriver.ChromeService(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=chrome_service)
 driver.get("http://www.google.com")
