df06914b1f071675525088c09360ea78fafa14d1
mcwilk <mcwilk@wp.pl>
2023-11-26 19:55:34 +0100

    Extract locators to separate file

--- FirstAppTestingWithPOP/pages/search_hotel.py
+++ FirstAppTestingWithPOP/pages/search_hotel.py
@@ -1,36 +1,28 @@
 from selenium.webdriver.common.by import By
 
+from FirstAppTestingWithPOP.locators.locators import SearchHotelLocators
+
 
 class SearchHotelPage:
 
     def __init__(self, driver):
         self.driver = driver
-        # Locators:
-        self.search_hotel_span_xpath = "//span[text()='Search by Hotel or City Name']"
-        self.search_hotel_input_xpath = "//div[@id='select2-drop']//input"
-        self.location_match_span_xpath = "//span[text()='Dubai']"
-        self.check_in_input_name = "checkin"
-        self.check_out_input_name = "checkout"
-        self.travellers_input_id = "travellersInput"
-        self.adult_input_id = "adultInput"
-        self.child_input_id = "childInput"
-        self.search_button_xpath = "//button[text()=' Search']"
 
     def set_city(self, city):
-        self.driver.find_element(By.XPATH, self.search_hotel_span_xpath).click()
-        self.driver.find_element(By.XPATH, self.search_hotel_input_xpath).send_keys(city)
-        self.driver.find_element(By.XPATH, self.location_match_span_xpath).click()
+        self.driver.find_element(By.XPATH, SearchHotelLocators.search_hotel_span_xpath).click()
+        self.driver.find_element(By.XPATH, SearchHotelLocators.search_hotel_input_xpath).send_keys(city)
+        self.driver.find_element(By.XPATH, SearchHotelLocators.location_match_span_xpath).click()
 
     def set_date_range(self, check_in, check_out):
-        self.driver.find_element(By.NAME, self.check_in_input_name).send_keys(check_in)
-        self.driver.find_element(By.NAME, self.check_out_input_name).send_keys(check_out)
+        self.driver.find_element(By.NAME, SearchHotelLocators.check_in_input_name).send_keys(check_in)
+        self.driver.find_element(By.NAME, SearchHotelLocators.check_out_input_name).send_keys(check_out)
 
     def set_travellers(self, adults, children):
-        self.driver.find_element(By.ID, self.travellers_input_id).click()
-        self.driver.find_element(By.ID, self.adult_input_id).clear()
-        self.driver.find_element(By.ID, self.adult_input_id).send_keys(adults)
-        self.driver.find_element(By.ID, self.child_input_id).clear()
-        self.driver.find_element(By.ID, self.child_input_id).send_keys(children)
+        self.driver.find_element(By.ID, SearchHotelLocators.travellers_input_id).click()
+        self.driver.find_element(By.ID, SearchHotelLocators.adult_input_id).clear()
+        self.driver.find_element(By.ID, SearchHotelLocators.adult_input_id).send_keys(adults)
+        self.driver.find_element(By.ID, SearchHotelLocators.child_input_id).clear()
+        self.driver.find_element(By.ID, SearchHotelLocators.child_input_id).send_keys(children)
 
     def perform_search(self):
-        self.driver.find_element(By.XPATH, self.search_button_xpath).click()
+        self.driver.find_element(By.XPATH, SearchHotelLocators.search_button_xpath).click()
--- FirstAppTestingWithPOP/pages/search_results.py
+++ FirstAppTestingWithPOP/pages/search_results.py
@@ -1,20 +1,19 @@
 from selenium.webdriver.common.by import By
 
+from FirstAppTestingWithPOP.locators.locators import SearchResultLocators
+
 
 class SearchResultsPage:
 
     def __init__(self, driver):
         self.driver = driver
-        # Locators:
-        self.hotel_names_xpath = "//h4[contains(@class, 'list_title')]//b"
-        self.hotel_prices_xpath = "//div[contains(@class, 'price_tab')]//b"
 
     def get_hotel_names(self):
-        hotels = self.driver.find_elements(By.XPATH, self.hotel_names_xpath)
+        hotels = self.driver.find_elements(By.XPATH, SearchResultLocators.hotel_names_xpath)
 
         return [hotel.get_attribute("textContent") for hotel in hotels]
 
     def get_hotel_prices(self):
-        prices = self.driver.find_elements(By.XPATH, self.hotel_prices_xpath)
+        prices = self.driver.find_elements(By.XPATH, SearchResultLocators.hotel_prices_xpath)
 
         return [price.get_attribute("textContent") for price in prices]
